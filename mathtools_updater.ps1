bbeb33683ba40f37e366ae5a49a1a07fe7b60327
Karl J <k.w.jankowski@gmail.com>
2021-09-22 08:49:48 -0400

    MathTools 086

--- mathtools_updater.ps1
+++ mathtools_updater.ps1
@@ -11,8 +11,8 @@ Write-Host "Alpha"
 ########################################################
 ## GLOBALS ##
 
-$global:mathtools_version = "3_0_1_085" #Example: "3_0_1_055"
-$global:mathtools_url = "http://movemen.com/files/downloads/mtv3/085" #do not add trailing slash, example: "http://movemen.com/files/downloads/mtv3/055"
+$global:mathtools_version = "3_0_1_086" #Example: "3_0_1_055"
+$global:mathtools_url = "http://movemen.com/files/downloads/mtv3/086" #do not add trailing slash, example: "http://movemen.com/files/downloads/mtv3/055"
 
 
 ########################################################
@@ -33,6 +33,7 @@ function downloadMathToolsCC($YYYY)
   Write-Host "DEBUG: Downloading MathTools for InDesign CC Server" $YYYY
   $mathtools = "MathToolsEESrv-$mathtools_version-CC-$YYYY-WIN64.zip"
   $url = "$mathtools_url/$mathtools"
+  Write-Host "DEBUG: MathTools URL is $url"
   $global:output_dir = "$PSScriptRoot\staging\mathtools\$YYYY"
   $output = "$output_dir\$mathtools"
   $start_time = Get-Date
@@ -63,6 +64,7 @@ function downloadMathTools($YYYY)
   Write-Host "DEBUG: Downloading MathTools for InDesign Server" $YYYY
   $mathtools = "MathToolsEESrv-$mathtools_version-$YYYY-WIN64.zip"
   $url = "$mathtools_url/$mathtools"
+  Write-Host "DEBUG: MathTools URL is $url"
   $global:output_dir = "$PSScriptRoot\staging\mathtools\$YYYY"
   $output = "$output_dir\$mathtools"
   $start_time = Get-Date
@@ -73,6 +75,36 @@ function downloadMathTools($YYYY)
   Write-Output "DEBUG: Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
     $verifyDownload = "$output_dir\MathToolsEESrv-$mathtools_version-$YYYY-WIN64.zip"
+    Write-Output "DEBUG: Check if $verifyDownload is here."
+    if (Test-Path $verifyDownload -PathType leaf)
+        {
+             Write-Warning "DEBUG: $verifyDownload exists."
+        }
+        else
+        {
+            Write-Warning "FAILURE: $verifyDownload MISSING."
+            [Environment]::Exit(1)
+
+        }
+
+}
+
+function downloadMathTools2021($YYYY)
+{
+  Write-Host "DEBUG: Downloading MathTools for InDesign Server" $YYYY
+  $mathtools = "MathToolsEESrv-$mathtools_version-2021_0-WIN64.zip" #temporary override for anomaly in URLs.
+  $url = "$mathtools_url/$mathtools"
+  Write-Host "DEBUG: MathTools URL is $url"
+  $global:output_dir = "$PSScriptRoot\staging\mathtools\$YYYY"
+  $output = "$output_dir\$mathtools"
+  $start_time = Get-Date
+
+  New-Item -ItemType directory -Path $output_dir -Force
+   Write-Output "DEBUG: URL for download is $url."
+  Invoke-WebRequest -Uri $url -OutFile $output
+  Write-Output "DEBUG: Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+    $verifyDownload = "$output_dir\MathToolsEESrv-$mathtools_version-2021_0-WIN64.zip"  #temporary override for anomaly in URLs.
     Write-Output "DEBUG: Check if $verifyDownload is here."
     if (Test-Path $verifyDownload -PathType leaf)
         {
@@ -156,6 +188,32 @@ function updateMathTools($YYYY)
 }
 
 
+function updateMathTools2021($YYYY)
+{
+    #call function
+    Write-Host "DEBUG: This is updateMathTools for IDS $YYYY"
+    Write-Host "DEBUG: Stopping InDesign CC $YYYY"
+    Stop-Service -Name "InDesignServerService x64"
+    Get-Process "InDesign*" | Stop-Process -Force
+    #call function
+    Write-Host "DEBUG: Backing up MathTools License $YYYY"
+    backupMathToolsLicense
+
+    #main body
+    #remove old MathTools
+    Remove-Item $idsLocation\Plug-Ins\movemen -Force -Recurse -ErrorAction SilentlyContinue
+
+    #production
+    Write-Host "DEBUG: Working with MathToolsEESrv-$mathtools_version-2021_0-WIN64.zip" #temporary override for name anomaly
+    Microsoft.PowerShell.Archive\Expand-Archive -Path $output_dir\MathToolsEESrv-$mathtools_version-2021_0-WIN64.zip -DestinationPath $idsLocation\Plug-Ins -Force #temporary override for name anomaly
+    Copy-Item  $output_dir\lic\*.lic -Destination $idsLocation\Plug-Ins\movemen\lic -Force  -ErrorAction SilentlyContinue
+
+    #call function
+    Write-Host "DEBUG: Starting InDesign CC $YYYY"
+    Start-Service -Name "InDesignServerService x64"
+
+}
+
 ######## FUNCTION ####################
 function detectMathTools
 {
@@ -207,7 +265,7 @@ Else
           13 {$idsYYYY = "2018"; "DEBUG: Adobe InDesign Server CC $idsYYYY"; downloadMathToolsCC "$idsYYYY"; updateMathToolsCC "$idsYYYY"}
           14 {$idsYYYY = "2019"; "DEBUG: Adobe InDesign Server CC $idsYYYY"; downloadMathToolsCC "$idsYYYY"; updateMathToolsCC "$idsYYYY"}
           15 {$idsYYYY = "2020"; "DEBUG: Adobe InDesign Server $idsYYYY"; downloadMathTools "$idsYYYY"; updateMathTools "$idsYYYY"}
-          16 {$idsYYYY = "2021"; "DEBUG: Adobe InDesign Server $idsYYYY"; downloadMathTools "$idsYYYY"; updateMathTools "$idsYYYY"}
+          16 {$idsYYYY = "2021"; "DEBUG: Adobe InDesign Server $idsYYYY"; downloadMathTools2021 "$idsYYYY"; updateMathTools2021 "$idsYYYY"}
       }
 
 
